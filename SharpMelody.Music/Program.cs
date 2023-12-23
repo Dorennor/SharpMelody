22abec56e9ad753a96e5cb6272038bf83d47287b
ostapsivko <o-stap@hotmail.com>
2023-12-23 20:17:13 +0200

    Save changed file

--- SharpMelody.Music/Program.cs
+++ SharpMelody.Music/Program.cs
@@ -14,31 +14,50 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//TODO auth
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+//TODO logging
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+//TODO routing
+
+//TODO endpoints
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// TODO VR
+//========================================================
+// files storage
+// - model band/singer { Name, Albums, MetaGenre, ID }
+// - album model { Songs, Singer, Genre(s), ID }
+// - song model { Title, Genre, Metadata, Band, Album, ID }
+// - metadata model { Dictionary<string, string> }
+
+// create folder for music
+// structure -> root => singer => albums => songs
+
+// Library Refresh
+// 1. on server startup
+// 2. refresh endpoint
+// OR file watcher
+
+// (I)MusicRepository
+// CR-U
+// *use TagSharp for metadata extraction on song add*
+// unit tests for edge cases + happy path
+// testing framework - xUnit, nUint
+// red - green - refactor
+// AAA pattern
+
+// endpoints for songs listing
+// - for singer
+// - for albums
+// - for song
+
+// TODO OS
+//=================================
+// EF Core -> add 
+// table for metadata for persistence
+// Research DB (PostgreSQL) for metadata storage
+// Songs metadata => artist | title | album | year | genre | length | filename
+// Genre/singer statistics
